1f5dbbed843ebfdc04b90254dbfb4396f1ffe973
EvgeniaKr <Evgenia@DESKTOP-5KCJKI3>
2022-03-17 13:45:03 +0300

    Initial commit

---
+++ ConsoleApp1/polygon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class polygon : circle
+    {
+        public virtual void Func()
+        {
+
+            Console.WriteLine("polygon(10, 10, 100, 100)");
+        }
+    }
+}
