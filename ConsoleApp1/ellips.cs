8a0874dcc5e6a3e38f88321a307b00adc7482a40
EvgeniaKr <Evgenia@DESKTOP-5KCJKI3>
2022-03-19 20:05:11 +0300

    Next commit

---
+++ ConsoleApp1/ellips.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class ellips : rectangle
+    {
+        public virtual void Func()
+        {
+
+            Console.WriteLine("ellips(10, 10, 10)");
+        }
+    }
+}
